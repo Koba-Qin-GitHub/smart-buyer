4795da3ecd3d912dad5776f7f4d52fa07b36b282
Koba <zkt.hobby@gmail.com>
2023-08-31 17:47:07 +0900

    マイページ機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+  def show
+    set_user
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+
+  private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+    
+
+
 end
