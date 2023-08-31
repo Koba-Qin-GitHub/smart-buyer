83755f9749595132b671d5f8c7450c8ec4b72226
Koba <zkt.hobby@gmail.com>
2023-08-31 14:47:58 +0900

    バリデーションの設定(Userモデル)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
+  validates :name, presence: true
+  validates :birthday, presence: true 
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
+
+  validates :company_id, numericality: { other_than: 1 } 
+
+
+
+
+  
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :company
-  
+
 end
