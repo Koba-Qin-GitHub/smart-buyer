2b72484b68d17c581a397a3097786c6c2244b3c8
Koba <zkt.hobby@gmail.com>
2023-09-01 17:27:21 +0900

    userモデル単体テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,9 @@ class User < ApplicationRecord
   validates :birthday, presence: true 
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には文字と数字の両方を入れてください', if: :password_required?       # パスワードの入力無しでユーザー情報を編集できる記述
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には半角英字、半角数字の両方を入れてください', if: :password_required?       # パスワードの入力無しでユーザー情報を編集できる記述
 
-  validates :company_id, numericality: { other_than: 1 } 
+  validates :company_id, numericality: { other_than: 1, message: 'を選択してください' }
 
 
 
