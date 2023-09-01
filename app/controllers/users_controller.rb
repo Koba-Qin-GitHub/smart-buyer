ae32c953af62c12a3918614ea5a7b3b54247865b
Koba <zkt.hobby@gmail.com>
2023-09-01 16:04:03 +0900

    ユーザー情報編集機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,19 @@ class UsersController < ApplicationController
     set_user
   end
 
+
   def edit
 
   end
 
+  
   def update
+
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
 
   end
 
@@ -17,6 +25,10 @@ class UsersController < ApplicationController
 
     def set_user
       @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :company_id, :birthday)
     end
     
 
