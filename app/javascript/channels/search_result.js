376c50781d9783bca76ef48fc526ee43f6c83d59
Koba <zkt.hobby@gmail.com>
2023-09-04 10:25:08 +0900

    WebAPIを利用したAjax　実装

---
+++ app/javascript/channels/search_result.js
@@ -0,0 +1,40 @@
+'use strict';
+
+// データ取得のイベント(単語入力  → 検索)
+window.onload = function () {
+  document.getElementById('search_form').onsubmit = function(event) { 
+    event.preventDefault();
+    const search_word = document.getElementById('search_form').word.value;
+    ajaxRequest(search_word );
+};}
+
+// データ取得(非同期通信)
+function ajaxRequest(search_word ) {
+  $.ajax({
+    url: 'https://api.mouser.com/api/v1/search/partnumber?apiKey=31e7f2b7-2345-4229-80fb-b48ba1ac23c6',
+    crossDomain: true,
+    method: 'post',
+    headers: {
+      'accept': 'application/json'
+    },
+    contentType: 'application/json',
+    data: JSON.stringify({
+      'SearchByPartRequest': {
+        'mouserPartNumber': search_word,
+        'partSearchOptions': ''
+      }
+    })
+  }).done(function(response) {
+    response_item_info(response);
+  });
+}
+
+// 検索ヒットが１件のみの場合
+function response_item_info (res) {
+  const result_PartNumber = res.SearchResults.Parts[0].ManufacturerPartNumber;
+  const result_Stock = res.SearchResults.Parts[0].FactoryStock;
+  document.getElementById('result_PartNumber').textContent = `正式品番：${result_PartNumber}`; 
+  document.getElementById('result_Stock').textContent = `在庫数量：${result_Stock}個`; 
+}
+
+// 検索ヒットが２件以上の場合
