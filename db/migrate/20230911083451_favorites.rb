f90d60cca4f19df5329040ba7a013630754eb550
Koba <zkt.hobby@gmail.com>
2023-09-11 17:37:04 +0900

    favoritesマイグレーションファイルの再作成

--- db/migrate/20230911083451_favorites.rb
+++ db/migrate/20230911083451_favorites.rb
@@ -1,7 +1,6 @@
-class CreateFavorites < ActiveRecord::Migration[6.0]
+class Favorites < ActiveRecord::Migration[6.0]
   def change
     create_table :favorites do |t|
-
 
       t.references :user,      null: false, foreign_key: true
       t.references :item,      null: false, foreign_key: true
