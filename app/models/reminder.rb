279568d38c7659c6591c882ce8e578cb4a70f5f1
Koba <zkt.hobby@gmail.com>
2023-09-27 21:16:17 +0900

    reminderバリデーション　実装

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,5 +1,8 @@
 class Reminder < ApplicationRecord
 
+  validates :case_id,           numericality: { other_than: 1, message: 'を選択してください' }
+  validates :reminder_way_id,   numericality: { other_than: 1, message: 'を選択してください' }
+
   belongs_to :favorite
 
   extend ActiveHash::Associations::ActiveRecordExtensions
