bb70113a146ad2e1580c60c111b26caa52abec6b
Koba <zkt.hobby@gmail.com>
2023-09-29 18:08:01 +0900

    Update schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -9,10 +9,11 @@ require File.expand_path(File.dirname(__FILE__) + "/environment")
 job_type :rake, "source /Users/y.takashi/.zshrc; export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output"
 
 # 環境変数'RAILS_ENV'にセットされている変数またはdevelopmentを指定
-rails_env = ENV['RAILS_ENV'] || :development
+# rails_env = ENV['RAILS_ENV'] || :development
 
 # cronを実行する環境変数をセット
-set :environment, rails_env
+# set :environment, rails_env
+set :environment, :production
 
 # cronのログの吐き出し場所
 set :output, "#{Rails.root}/log/cron.log"
@@ -20,7 +21,7 @@ set :output, "#{Rails.root}/log/cron.log"
 
 
 
-every 5.minutes do
+every 1.minutes do
   rake "api_date_save:Mouser_API_date_save"
   rake "reminder:reminder_mail"
 end 
