4e8ddecedd1882ccf493b9143059f917ad87d7b7
Koba <zkt.hobby@gmail.com>
2023-10-02 13:22:04 +0900

    routes　リファクタリング

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#new'
   
-  resources :users
+  resources :users, only:[:show, :edit, :update]
 
-  resources :items do
-    resources :favorites
+  resources :items, only:[:index, :new, :create, :show] do
+    resources :favorites, only:[:create, :destroy]
 
   end
 
-  resources :favorites do
-    resources :reminders
+  resources :favorites, only:[:create, :destroy] do
+    resources :reminders, only:[:new, :create, :destroy]
   end
   
 
